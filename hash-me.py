cb41f780340c25ac6c99d3b9e74c0b4ff1e53211
Jay Ashok Lal <jay.lal@somaiya.edu>
2017-08-12 13:37:25 +0530

    Added cmd-line, modularized,py2&3 compatible

--- hash-me.py
+++ hash-me.py
@@ -1,41 +1,98 @@
+#!/usr/bin/python3
+
+from __future__ import print_function
+
 import hashlib
 import os,sys
 
-if len(sys.argv)==1:
-	mydir = os.getcwd()
-else:
-	mydir = sys.argv[1]
+
+def handle_command_line_args():
+	IncludeEmpty = False
+	mydir = ''
+
+	#Only 1 agrument
+	if len(sys.argv)==1:
+		mydir = os.getcwd()
+
+	#More than 1 arguments..
+	elif '-0' in sys.argv:
+		IncludeEmpty = True
+		#Check if 3 arguments are passed
+		if len(sys.argv)==3:
+			#Is '-0' the 2nd argument?
+			if sys.argv[1] == '-0':
+				mydir = sys.argv[2]
+			#'-0' the 3rd/last argument?
+			else:
+				mydir = sys.argv[1]
+
+		#Only 2 arguments, no directory specified
+		else:
+			mydir = os.getcwd()
+	#More than 1 arguments but no '-0'
+	else:
+		mydir = sys.argv[1]
+
+	return mydir,IncludeEmpty
 
 def md5(fname):
     hash_md5 = hashlib.md5()
+
+    #Open the file
     with open(fname, "rb") as f:
+        #Read data in chunks of '4096' bytes
         for chunk in iter(lambda: f.read(4096), b""):
             hash_md5.update(chunk)
     return hash_md5.hexdigest()
 
-file_list = os.listdir(mydir)
 
-seen_files = dict()
-duplicate = dict()
+def find_duplicate(mydir,IncludeEmpty):
+	#List all the file_names in the specified directory
+	file_list = os.listdir(mydir)
 
-for f in file_list:	
-	Fname = os.path.join(mydir,f)
-	#Ignore directories
-	if os.path.isdir(Fname):
-		continue
-	#Ignore Linux-Backup files
-	if f.endswith('~'):
-		continue
-	#Ignore Empty Files
-	if os.path.getsize(Fname)==0:
-		continue
-	Fhash = md5(Fname)
-	if Fhash in seen_files:
-		duplicate[seen_files[Fhash]] = f
+	#Keep a dictionary/hashtable of all seen and duplicate files
+	seen_files = dict()
+	duplicate = dict()
+
+	for f in file_list:	
+		#Get the complete path of file
+		Fname = os.path.join(mydir,f)
+		#Ignore directories
+		if os.path.isdir(Fname):
+			continue
+		#Ignore Linux-Backup files
+		if f.endswith('~'):
+			continue
+		#Ignore Empty Files
+		if os.path.getsize(Fname)==0 and not IncludeEmpty:
+			continue
+		#Calculate hash
+		Fhash = md5(Fname)
+
+		if Fhash in seen_files:
+			duplicate[seen_files[Fhash]] = f
+		else:
+			seen_files[Fhash] = f
+
+	return duplicate
+
+def display(duplicate,mydir):
+	print('Checking for duplicate files in',mydir)
+	if duplicate:
+		print('Found',len(duplicate),'duplicates')
+		for f in duplicate:
+			print("'{}' is a possible duplicate of '{}'".format(duplicate[f],f))
 	else:
-		seen_files[Fhash] = f
+		print('None Found!')		
 
-print('Checking for duplicate...')
-for f in duplicate:
-	print(duplicate[f],'is a possible duplicate of',f)
 
+def main():
+
+	mydir,IncludeEmpty = handle_command_line_args()
+
+	duplicate_files = find_duplicate(mydir,IncludeEmpty)
+
+	display(duplicate_files,mydir)
+
+if __name__=='__main__':
+	main()
